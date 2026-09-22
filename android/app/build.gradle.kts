fede576dfa76a44b637000b5d67d39f6331ce341
TravelGo Developer <developer@travelgo.app>
2026-09-22 19:10:56 +0100

    fix(android): upgrade compileSdk to 36 and Android Gradle Plugin to 8.9.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.travelgo.app"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -17,7 +17,7 @@ android {
     defaultConfig {
         applicationId = "com.travelgo.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
